c6722befb04bdb6b3b534e6d6754cf1d4ae0943c
Kartik <kartikk189@gmail.com>
2023-11-29 10:31:33 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import React, { useState } from 'react'
 
 function App() {
   const[count, setCount]= useState(0)
+
+  const inc = () => {
+    setCount(count+1)
+  }
+const dec = () => {
+  setCount(count-1)
+}
+
   return (
     <div>
       <h1>Counting Data</h1>
       <h1>{count}</h1>
-      <button onClick={()=>setCount(count+1)}>+</button>
-      <button onClick={()=>setCount(count-1)}>-</button>
+      <button onClick={inc}>+</button>
+      <button onClick={dec}>-</button>
     </div>
   )
 }
